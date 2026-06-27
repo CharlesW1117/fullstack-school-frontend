d42d57885b6f181156e65e145fa6b0b768988490
Charles Wade <Charleskwadejr1088@gmail.com>
2026-06-27 17:27:00 -0500

    fixed merge conflict

--- src/api/teams.js
+++ src/api/teams.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // conection
 const API = import.meta.env.VITE_API;
@@ -49,9 +48,3 @@ export async function deleteTeam(id, token) {
     headers: { Authorization: `Bearer ${token}` },
   })
 }
-=======
-import { request } from "./client";
-
-export const getTeams = () => request("/teams", { method: "GET" });
-export const getTeam = (id) => request(`/teams/${id}`, { method: "GET" });
->>>>>>> b3e02669d8d9c5b1fcb35b331ebda3975ea6e4f3
--- src/api/users.js
+++ src/api/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //register a user 
 export async function register(username, password) {
   const res = await fetch('http://localhost:3000/users/register', {
@@ -18,12 +17,3 @@ export async function login(username, password) {
   })
   return res.text()
 }
-=======
-import { request } from "./client";
-
-export const register = (payload) =>
-  request("/users/register", { method: "POST", body: payload });
-
-export const login = (payload) =>
-  request("/users/login", { method: "POST", body: payload });
->>>>>>> b3e02669d8d9c5b1fcb35b331ebda3975ea6e4f3
